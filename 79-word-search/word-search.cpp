4e7365177b87094de310366262883e0f62136978
zulfahmi14 <zulfahmi.habibi@tiket.com>
2024-08-13 03:38:18 +0700

    Time: 1385 ms (7.13%) | Memory: 12.2 MB (12.30%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -7,31 +7,31 @@ public:
 
         for(int i = 0;i < row;i++) {
             for(int j = 0;j < col;j++) {
-                if(dfs(board, word, f, i,j)) return true;
+                if(dfs(board, word, f, i, j, 1)) return true;
             }
         }
 
         return false;
     }
 
-    bool dfs(vector<vector<char>>& board, string word, vector<vector<bool>>& f, int row, int col) {
+    bool dfs(vector<vector<char>>& board, string word, vector<vector<bool>>& f, int row, int col, int valid) {
         if(row < 0 || col < 0 || row >= board.size() || col >= board[0].size()) {
             return false;
         }
 
-        if(f[row][col] || board[row][col] != word[0]) { // visited
+        if(f[row][col] || board[row][col] != word[valid-1]) { // visited || not same
             return false ;
         }
 
-        if (word.length() == 1 && board[row][col] == word[0]) {
+        if (word.length() == valid) {
             return true ;
         }
 
         f[row][col] = true ;
-        bool a1 = dfs(board, word.substr(1, word.length()-1), f, row+1, col);
-        bool a2 = dfs(board, word.substr(1, word.length()-1), f, row-1, col);
-        bool a3 = dfs(board, word.substr(1, word.length()-1), f, row, col+1);
-        bool a4 = dfs(board, word.substr(1, word.length()-1), f, row, col-1);
+        bool a1 = dfs(board, word, f, row+1, col, valid+1);
+        bool a2 = dfs(board, word, f, row-1, col, valid+1);
+        bool a3 = dfs(board, word, f, row, col+1, valid+1);
+        bool a4 = dfs(board, word, f, row, col-1, valid+1);
         f[row][col] = false ; // backtracking
 
         return a1 || a2 || a3 || a4;
