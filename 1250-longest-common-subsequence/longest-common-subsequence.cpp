9dd670727261fc388d397fdb3beea25657f7c696
zulfahmi14 <zulfahmi.habibi@tiket.com>
2024-08-15 09:25:12 +0700

    Time: 48 ms (8.92%) | Memory: 26.2 MB (7.26%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    int arr[1001][1001] ;
-    
-    int dp(string &text1, string &text2, int idx1, int idx2) {
-        if (idx1 >= text1.length() || idx2 >= text2.length()) return 0 ;
-        if (arr[idx1][idx2] != -1) return arr[idx1][idx2] ;
-        
-        int ans = 0 ;
-        if (text1[idx1] == text2[idx2]) {
-            ans = 1 + dp(text1, text2, idx1+1, idx2+1);
-        } else {
-            ans += max(dp(text1, text2, idx1+1, idx2), dp(text1, text2, idx1, idx2+1));            
-        }
-        
-        arr[idx1][idx2] = ans ;
-               
-        return ans ;
-    }
-    
+    // int v[1001][1001] ;
+
     int longestCommonSubsequence(string text1, string text2) {
-        memset(arr,-1,1001*1001*sizeof(int));
-        return dp(text1, text2, 0, 0) ;
+        vector<vector<int>> v(text1.length(), vector<int>(text2.length(), -1));
+        // memset(v,-1,1001*1001*sizeof(int));
+        return dp(text1, text2, v, 0, 0);
+    }
+
+    int dp(string& text1, string& text2, vector<vector<int>>& v, int idx1, int idx2) {
+        if (idx1 >= text1.length() || idx2 >= text2.length()) return 0;
+        if (v[idx1][idx2] != -1) return v[idx1][idx2];
+
+        int ans;
+        if (text1[idx1] == text2[idx2]) {
+            ans = 1+dp(text1, text2, v, idx1+1, idx2+1);
+        } else {
+            ans = max(dp(text1, text2, v, idx1+1, idx2), dp(text1, text2, v, idx1, idx2+1));
+        }
+
+        v[idx1][idx2] = ans;
+
+        return ans;
     }
 };
