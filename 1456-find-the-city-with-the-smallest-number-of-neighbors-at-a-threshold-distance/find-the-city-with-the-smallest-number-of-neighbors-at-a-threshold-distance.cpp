5ea75a0a23054b67757b432f5856d0cfa92db69c
zulfahmi14 <zulfahmi.habibi@tiket.com>
2024-08-15 10:13:25 +0700

    Time: 142 ms (12.07%) | Memory: 32.6 MB (10.62%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,38 +1,72 @@
 class Solution {
 public:
-    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        long maxInt = INT_MAX;
-        vector<vector<long>> m(n+1, vector<long>(n+1, maxInt*n)); // -1 means infinite.
-        long ans = -1;
-        long smallestDist = maxInt*n;
 
-        for(auto i : edges) {
-            m[i[0]][i[1]] = i[2];
-            m[i[1]][i[0]] = i[2];
-        }
+    // struct cmp {
+    //     bool operator(pair<int,int>& a, pair<int,int>& b) {
+    //         return a.first < b.first;
+    //     }
+    // }
 
-        for (int i = 0; i < n; i++) 
-            m[i][i] = 0;
 
-        for(int k =0; k < n ; k++) {
-            for(int i=0; i < n ; i++) {
-                for(int j=0; j< n ; j++) {
-                    m[i][j] = min(m[i][j], m[i][k] + m[k][j]);
+    int reachCity(int n, unordered_map<int, unordered_map<int, int>>& m, int city, int limit) {
+        priority_queue<pair<int,int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
+        vector<bool> v(n, false);
+        int ans = 0;
+        q.push({0, city});
+
+        while(!q.empty()) {
+            pair<int,int> nextCity = q.top();
+            q.pop();
+
+            if(v[nextCity.second] || nextCity.first > limit) {
+                continue;
+            }
+
+            v[nextCity.second] = true;
+            if(nextCity.second != city && nextCity.first<= limit) ans++;
+
+
+            // if(city == 4) {
+            //         cout << nextCity.first << " " << nextCity.second << endl;
+            //     }
+
+            for(auto mm : m[nextCity.second]) {
+
+                if (mm.second+nextCity.first <= limit) {
+                    q.push({mm.second+nextCity.first, mm.first});
+                    // if (!v[mm.first]) {
+                    //     v[mm.first] = true;
+                    //     ans++;
+                    // }
                 }
             }
         }
 
-        for(int i =0; i < n ; i++) {
-            long sum = -1;
-            for(int j = 0 ; j<n ; j++) {
-                if (m[i][j] <= distanceThreshold) {
-                    sum++;
-                } 
-            }
-            
-            if (sum <= smallestDist) {
-                smallestDist = sum;
-                ans = i;
+        return ans ;
+    }
+
+
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        unordered_map<int, unordered_map<int, int>> m; // weight from city i to city j
+        vector<int> v;
+        int ans = -1;
+
+        for(int i=0 ; i<edges.size() ;i++) {
+            m[edges[i][0]][edges[i][1]] = edges[i][2];
+            m[edges[i][1]][edges[i][0]] = edges[i][2];
+        }
+
+        for(int i=0;i<n;i++) {
+            v.push_back(reachCity(n, m, i, distanceThreshold));
+        }
+
+        int minCity = INT_MAX;
+
+
+        for(int i=0;i<n;i++) { 
+            cout << v[i] << " ";
+            if(v[i] <= minCity) {
+                minCity = v[i]; ans = i;
             }
         }
 
@@ -40,5 +74,7 @@ public:
     }
 };
 
-// by floyd warshall
-// create matrix n-n
+
+// loop each city
+// convert edge to 2D map
+// do dijkstra, reach asmuch as possible cities
