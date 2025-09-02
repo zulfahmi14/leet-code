a6857a3484bd473d1c5b54c238e4895167263d88
zulfahmi14 <zulfahmi.habibi@tiket.com>
2025-09-02 21:30:09 +0700

    Time: 31 ms (66.26%) | Memory: 59.5 MB (42.34%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,25 +1,33 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        Deque<Integer> dq = new ArrayDeque<>();
-        int ans[] = new int[nums.length-k+1];
-
-        for(int i = 0 ; i < nums.length ; i++) {
-            // keep sliding windows = K
-            if (!dq.isEmpty() && i >= k && dq.getFirst() < i-k+1) { // i = 1, k = 1.
-                dq.removeFirst();
+        Deque<Integer> subArray = new ArrayDeque<>();
+        int lengthAns = nums.length - k+1;
+        int[] ans = new int[lengthAns];
+        
+        for(int i = 0 ; i < nums.length ; i++) {    
+            // maintain the sliding windows size = k
+            while(!subArray.isEmpty() && subArray.getLast() < i-k+1) {
+                subArray.removeLast();
             }
 
-            while (!dq.isEmpty() && nums[dq.getLast()] < nums[i]) {
-                dq.removeLast();
+            // maintain highest value.
+            while(!subArray.isEmpty() && nums[subArray.getFirst()] <= nums[i]) {
+                subArray.removeFirst();
             }
 
-            dq.addLast(i);
+            subArray.addFirst(i);
 
             if (i >= k-1) {
-                ans[i-k+1] = nums[dq.getFirst()];
+                ans[i-k+1] = nums[subArray.getLast()];
             }
         }
 
         return ans;
     }
 }
+
+// 1 2 3 4 5
+
+// i = 4
+// k = 3
+// last = 2
